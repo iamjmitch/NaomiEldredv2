e7fee4a474c668fd8a521fe05f50802e110e3afe
James Mitchell <iamjmitch1990@gmail.com>
2020-12-05 01:24:18 +1000

    keywords added

--- src/components/global/SEO.js
+++ src/components/global/SEO.js
@@ -11,6 +11,7 @@ function SEO({ description, lang, meta, title }) {
           siteMetadata {
             title
             description
+            keywords
           }
         }
       }
@@ -31,6 +32,10 @@ function SEO({ description, lang, meta, title }) {
         {
           name: `description`,
           content: metaDescription,
+        },
+        {
+          name: `keywords`,
+          content: site.siteMetadata.keywords,
         },
         {
           property: `og:title`,
