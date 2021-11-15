d042be043c926afa775ea2123620fcda52b325ba
James Mitchell <iamjmitch1990@gmail.com>
2021-11-15 20:38:59 +1000

    lowercase

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -24,15 +24,15 @@ const ContactPage = props => {
         data-sal-easing="ease"
         other="b{font-weight:600;color:rgb(196, 70, 119); font-size:1.2em;}"
       >
-        <b>Naomi Is Not Currently Taking On Any New Clients</b>
+        <b>Naomi is not currently taking on any new clients</b>
         <br></br>
         <br></br>
-        Due To A Large Number Of Recent Bookings, Naomi Is Currently Unable To
-        Take On Any New Bookings. Your Interest In Naomi's Services Are Greatly
-        Appreciated
+        Due to a large number of recent bookings, Naomi is currently unable to
+        take on any new bookings. Your interest nn Naomi's services are greatly
+        appreciated
         <br></br>
         <br></br>
-        <b>This Page Will Be Updated Once New Bookings Are Being Accepted</b>
+        <b>This page will be updated once new bookings sre being accepted</b>
       </Paragraph>
       {/* <ContactForm /> */}
     </Layout>
